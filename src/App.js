eee7c8ab17bdb85767c939bfe72782f87540ded1
Jordan Trempert <32543343+jordan-trempert@users.noreply.github.com>
2021-09-08 09:12:56 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -95,6 +95,24 @@ function Home() {
         <span id="hover" onClick={egg10}>
           <div className="ds10">
             <img
+              src="https://raw.githubusercontent.com/jordan-trempert/media/main/Perspective%202%20logo.png"
+              alt="dslogo"
+              id="dslogo"
+            />
+            <br />
+            <span id="dscontent">
+              Perspective Part 2<br />
+              
+              Coming Soon!
+              <br />
+            </span>
+          </div>
+        </span>
+        <br />
+        <br />
+        <span id="hover" onClick={egg10}>
+          <div className="ds10">
+            <img
               src="https://raw.githubusercontent.com/jordan-trempert/media/main/Perspective.png"
               alt="dslogo"
               id="dslogo"
