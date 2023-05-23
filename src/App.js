9adc1bd89520cb9a36e94f0fdce8abba1cb95963
Jordan Trempert <32543343+jordan-trempert@users.noreply.github.com>
2023-05-23 09:22:40 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -78,13 +78,18 @@ const checkKonamiCode = useCallback((event) => {
   setKonamiCodeIndex((prevKonamiCodeIndex) => prevKonamiCodeIndex + 1);
 
   if (konamiCodeIndex + 1 === konamiCode.length) {
-    const randomNumber = Math.random();
-    const redirectUrl =
-      randomNumber < 0.5
-        ? 'https://stardle.stardomga.me'
-        : 'https://mario.stardomga.me';
-    window.location.href = redirectUrl;
+  const randomNumber = Math.random();
+  let redirectUrl;
+
+  if (randomNumber < 0.33) {
+    redirectUrl = 'https://stardle.stardomga.me';
+  } else if (randomNumber < 0.67) {
+    redirectUrl = 'https://mario.stardomga.me';
+  } else {
+    redirectUrl = 'https://pong.stardomga.me';
   }
+
+  window.location.href = redirectUrl;
 }, [konamiCodeIndex]);
 
 
