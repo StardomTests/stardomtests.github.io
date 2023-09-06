d5be90e2acf4b33ce18c828639b36247ab1f6afd
Jordan Trempert <32543343+jordan-trempert@users.noreply.github.com>
2023-09-06 10:51:59 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -312,7 +312,7 @@ const checkKonamiCode = useCallback((event) => {
             <br />
                   <span id="dscontent">
               {description12}<br />
-                <a href="https://stardomgames.itch.io/gravity-grapple">Public Beta 3</a> | <a href="https://gravitygrapple.stardomga.me">Web</a>
+                <a href="https://stardomgames.itch.io/gravity-grapple">Public Beta 4</a> | <a href="https://gravitygrapple.stardomga.me">Web</a>
       
               
              
